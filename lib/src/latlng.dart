c575aa09e0593ca1e64a711f800674ff0aad8ed6
Peter Siegmund <developer@mars3142.org>
2020-05-19 23:03:17 +0200

    add latlng testing

--- lib/src/latlng.dart
+++ lib/src/latlng.dart
@@ -19,53 +19,34 @@ class LatLng {
   final double _longitude;
 
   /// main constructor
-  const LatLng(this._latitude, this._longitude);
+  const LatLng(double latitude, double longitude)
+      : _latitude = latitude,
+        _longitude = longitude;
 
   /// formatted latitude
-  String latitude() {
+  String get latitude {
     return _format(_latitude, 'NS');
   }
 
   /// formatted longitude
-  String longitude() {
-    return _format(_longitude, 'WE');
+  String get longitude {
+    return _format(_longitude, 'EW');
   }
 
   String _format(double value, String suffix) {
-    while (value < -180.0) {
-      value += 360.0;
-    }
-
-    while (value > 180.0) {
-      value -= 360.0;
-    }
-
-    //switch the value to positive
-    var _isNegative = value < 0;
+    final isNegative = value < 0;
     value = value.abs();
 
-    //gets the degree
-    var _degrees = value.floor();
-    var delta = value - _degrees;
+    final degrees = value.floor();
+    final minutes = (value - degrees) * 60.0;
 
-    //gets minutes and seconds
-    var seconds = (3600.0 * delta).floor();
-    var _seconds = seconds % 60;
-    var _minutes = (seconds / 60.0).floor();
-    delta = delta * 3600.0 - seconds;
-
-    //gets fractions
-    var _milliseconds = (1000.0 * delta).toInt();
-
-    /// N 53° 25.831 E 010° 05.408
     var result = '';
-    if (_isNegative) {
+    if (isNegative) {
       result += suffix.substring(1);
     } else {
       result += suffix.substring(0, 1);
     }
-    result +=
-        " $_degrees° ${_minutes.toString().padLeft(2, '0')}' ${_seconds.toString().padLeft(2, '0')}\".${_milliseconds.toString().padLeft(3, '0')} ";
+    result += " $degrees° ${minutes.toStringAsFixed(3)}";
     return result;
   }
 }
---
+++ test/latlng_test.dart
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Peter Siegmund
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
+import 'dart:math';
+
+import 'package:test/test.dart';
+import 'package:freewig/freewig.dart';
+
+void main() {
+  group('LatLng testing', () {
+    test('Hamburg', () {
+      var location = const LatLng(53.5584902, 9.7877407);
+      expect('N 53° 33.509', location.latitude);
+      expect('E 9° 47.264', location.longitude);
+    });
+
+    test('New York', () {
+      var location = const LatLng(40.6974034, -74.1197633);
+      expect('N 40° 41.844', location.latitude);
+      expect('W 74° 7.186', location.longitude);
+    });
+
+    test('Sydney', () {
+      final location = const LatLng(-33.847927, 150.651794);
+      expect('S 33° 50.876', location.latitude);
+      expect('E 150° 39.108', location.longitude);
+    });
+  });
+}
